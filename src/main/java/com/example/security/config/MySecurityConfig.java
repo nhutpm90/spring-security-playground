631251aa865a344724fc04893d17d417a88298b5
nhut_pham <nhut.pham@ilogic.vn>
2022-07-02 21:11:51 +0700

    using JdbcUserDetailsManager

--- src/main/java/com/example/security/config/MySecurityConfig.java
+++ src/main/java/com/example/security/config/MySecurityConfig.java
@@ -1,15 +1,15 @@
 package com.example.security.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
@@ -26,34 +26,13 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 		.formLogin().and()
 		.httpBasic();
-		
-//		http.authorizeRequests().anyRequest().denyAll().and()
-//		.formLogin().and()
-//		.httpBasic();
-		
-//		http.authorizeRequests().anyRequest().permitAll().and()
-//		.formLogin().and()
-//		.httpBasic();
 	}
 	
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.inMemoryAuthentication()
-//			.withUser("admin").password("123456").authorities("admin").and()
-//			.withUser("user").password("123456").authorities("read").and()
-//			.passwordEncoder(NoOpPasswordEncoder.getInstance());
-//	}
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-		UserDetails user = User.withUsername("admin").password("123456").authorities("admin").build();
-		UserDetails user1 = User.withUsername("user").password("123456").authorities("read").build();
-		userDetailsService.createUser(user);
-		userDetailsService.createUser(user1);
-		auth.userDetailsService(userDetailsService);
+	@Bean
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
 	}
-
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
