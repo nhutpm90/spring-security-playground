d53ccdf6999044399d0046ff30afa6512e6eef6d
nhut_pham <nhut.pham@ilogic.vn>
2022-07-04 16:58:48 +0700

    add CORS CSRF

--- src/main/java/com/example/security/api/MainController.java
+++ src/main/java/com/example/security/api/MainController.java
@@ -3,6 +3,7 @@ package com.example.security.api;
 import java.util.Date;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,20 @@ public class MainController {
 	@GetMapping("/live-check")
 	public String liveCheck() throws Exception {
 		return "liveCheck:: " + new Date();
+	}
+	
+	@PostMapping("/test-post-request")
+	public String testPostRequest() throws Exception {
+		return "testPostRequest:: " + new Date();
+	}
+	
+	@GetMapping("/test-post-request01")
+	public String testPostRequest01() throws Exception {
+		return "testPostRequest01:: " + new Date();
+	}
+	
+	@PostMapping("/test-post-request02")
+	public String testPostRequest02() throws Exception {
+		return "testPostRequest02:: " + new Date();
 	}
 }
--- src/main/java/com/example/security/config/MySecurityConfig.java
+++ src/main/java/com/example/security/config/MySecurityConfig.java
@@ -1,18 +1,38 @@
 package com.example.security.config;
 
+import java.util.Collections;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http.cors().configurationSource(new CorsConfigurationSource() {
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				CorsConfiguration config = new CorsConfiguration();
+//				config.setAllowedOrigins(Collections.singletonList("http://172.16.2.29:3000"));
+				config.addAllowedOriginPattern(CorsConfiguration.ALL);
+				config.setAllowedMethods(Collections.singletonList("*"));
+				config.setAllowCredentials(true);
+				config.setAllowedHeaders(Collections.singletonList("*"));
+				config.setMaxAge(3600L);
+				return config;
+			}
+		}).and().csrf().ignoringAntMatchers("/test-post-request02").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+		.authorizeRequests()
 			.antMatchers("/myAccount").authenticated()
 			.antMatchers("/myBalance").authenticated()
 			.antMatchers("/myLoans").authenticated()
